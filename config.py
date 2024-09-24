a4219780e798722573176fbcdc215d1b310d1d88
apkomarla <aparna.komarla+2@gmail.com>
2024-09-24 13:57:17 -0700

    Updating input url to offenses_data repo

--- config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-id_label = "CDCNo"
-base_path = "C:/Users/apkom/prison_pop/"
-eval_count = 50000
-parallel = True
-to_excel = True
--- run.py
+++ run.py
@@ -1,50 +1,54 @@
 # -*- coding: utf-8 -*-
-import config
 from scenarios import rules
 from scenarios import general
 import sys
-
-# Eligibility model
-sys.path.insert(0, "C:/Users/apkom/resentencing_data_initiative/eligibility_model/code")
-import importlib
-import helpers
-import utils
-import extract 
-import eligibility
-import summary
+import yaml
 import pandas as pd
 import numpy as np
 import datetime
 from tqdm import tqdm
 import copy
 import os
+import importlib
+
+# Eligibility model
+sys.path.insert(0, "C:/Users/apkom/resentencing_data_initiative/eligibility_model/code")
+import helpers
+import utils
+import extract 
+import eligibility
+import summary
+
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
 # Extract relevant datasets
-print("Extracting demographics, current commits and prior commits datasets")
-demographics = pd.read_excel(config.base_path+"/data/input/Demographics_CDCR.xlsx")
-current_commits = pd.read_excel(config.base_path+"/data/input/CurrentCommitments_CDCR.xlsx")
-prior_commits = pd.read_excel(config.base_path+"/data/input/PriorCommitments_CDCR.xlsx")
+print(f"Extracting demographics, current commits and prior commits datasets from {config[base_path]}")
+demographics = pd.read_excel(config[base_path]+"Demographics_CDCR.xlsx", engine='openpyxl')
+current_commits = pd.read_excel(config[base_path]+"CurrentCommitments_CDCR.xlsx", engine='openpyxl')
+prior_commits = pd.read_excel(config[base_path]+"PriorCommitments_CDCR.xlsx", engine='openpyxl')
 
-print("Extracting a sample population for eligibility evaluation")
+print(f"Extracting a sample population of size {config[eval_count]} for eligibility evaluation")
 # Randomly select individuals to evaluate (as opposed to evaluating the entire population)
-sample_ids = list(demographics[config.id_label].sample(n=config.eval_count, random_state=1))
+sample_ids = list(demographics[config[id_label]].sample(n=config[eval_count], random_state=1))
 # Extract the sampled CDCR IDs
-sample_demographics = demographics[demographics[config.id_label].isin(sample_ids)]
-sample_current_commits = current_commits[current_commits[config.id_label].isin(sample_ids)]
-sample_prior_commits = prior_commits[prior_commits[config.id_label].isin(sample_ids)]
+sample_demographics = demographics[demographics[config[id_label]].isin(sample_ids)]
+sample_current_commits = current_commits[current_commits[config[id_label]].isin(sample_ids)]
+sample_prior_commits = prior_commits[prior_commits[config[id_label]].isin(sample_ids)]
 
 # Extract offense classifications
-sorting_criteria = pd.read_excel(config.base_path+"/offense_classification/selection_criteria.xlsx")
+sorting_criteria = pd.read_excel(config[base_path]+"/offense_classification/selection_criteria.xlsx")
 
-if __name__ == "__main__":
-    errors, adult_el_cdcr_nums = eligibility.gen_eligibility(demographics = sample_demographics, 
-                                                             sorting_criteria = sorting_criteria,
-                                                             current_commits = sample_current_commits, 
-                                                             prior_commits = sample_prior_commits, 
-                                                             write_path = config.base_path+"/data/output",
-                                                             eligibility_conditions = general.el_cond,
-                                                             pop_label = general.el_cond['population'],
-                                                             id_label = config.id_label, 
-                                                             comp_int = rules.comp_int,
-                                                             to_excel = config.to_excel, 
-                                                             parallel = config.parallel)
+
+errors, adult_el_cdcr_nums = eligibility.gen_eligibility(demographics = sample_demographics,
+                                                         sorting_criteria = sorting_criteria,
+                                                         current_commits = sample_current_commits,
+                                                         prior_commits = sample_prior_commits,
+                                                         write_path = "/data/output",
+                                                         eligibility_conditions = general.el_cond,
+                                                         pop_label = general.el_cond['population'],
+                                                         id_label = config[id_label],
+                                                         comp_int = rules.comp_int,
+                                                         to_excel = config[to_excel],
+                                                         parallel = config[parallel])
